c0c7b22bb5af0b4f4fa01887ec012836061a1341
papajose8 <jmrl98@gmail.com>
2022-08-01 18:08:09 +0200

    New version

--- Studies/betTradePercentage.py
+++ Studies/betTradePercentage.py
@@ -4,57 +4,64 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-percWonTrades = 52; # %
-initialInvestment = 15000; # u.m.
+maxWon = 60 # %
+minWon = 45 # %
+div = 2
+quan = (maxWon - minWon) * div + 1
+percWonTrades = [minWon + i / 2 for i in range(quan)] # %
+
+initialInvestment = 15000 # u.m.
 isComission = True
-makerComission = 0.018; # %
+makerComission = 0.018 # %
 maxTakeProfitPer = 10 # %
 
-# STUDY LIMITS DEFINITION
+# LIMITS DEFINITION
 maxPer = 50 # Maximum percentage
 minPer = 0 # Minimum percentage
 numDiv = 2 # Number of divisions in each % unit
 quantity = (maxPer - minPer) * numDiv
-numTrades = 100000
-numIt = 1
+perBetTrade = [(maxPer - minPer) * (i + 1) / quantity for i in range(quantity)]
 
-perBetTrade = []
+numTrades = 10
+numIt = 2;
 
-for i in range(quantity):
-    percentage = (maxPer - minPer) * (i + 1) / quantity
-    perBetTrade.append(percentage)
+results = [ [0, 0, 0, 0] for i in range(len(percWonTrades) * len(perBetTrade) * numIt)]
+countPos = -1
 
-ROI = []
-ROI = pd.DataFrame(ROI, columns=['Return'])
-winPer = []
-winPer = pd.DataFrame(winPer, columns=['Win Percentage'])
-betTradeCount = 0
+for botPerc in percWonTrades:
 
-for per in perBetTrade:
-    betTradeCount += 1
-    for i in range(numIt):
-        currentCapital = initialInvestment
-        winCount = 0
+    for per in perBetTrade:
+        per = per / 100 # unitary percentage
 
-        for j in range(numTrades):
-            if isComission:
-                betTrade = currentCapital * per * (1 - makerComission/100)
-            else:
-                betTrade = currentCapital * per;
-            winLossNum = random.uniform(0, 100)
-            if winLossNum > percWonTrades:
-                profitPer = - random.uniform(0, maxTakeProfitPer)
-            else:
-                profitPer = random.uniform(0, maxTakeProfitPer)
-                winCount += 1
-            if isComission:
-                finalTrade = betTrade * (1 + profitPer) * (1 - makerComission/100)
-            else:
-                finalTrade = betTrade * (1 + profitPer)
-            currentCapital = currentCapital + finalTrade
-        print(betTradeCount)
-        ROI['Return'][int(betTradeCount)] = (currentCapital - initialInvestment) / initialInvestment * 100
-        winPer['Win Percentage'][betTradeCount] = winCount / numTrades * 100
+        for i in range(numIt):
+            currentCapital = initialInvestment
+            winCount = 0
+
+            for j in range(numTrades):
+                if isComission:
+                    betTrade = currentCapital * per * (1 - makerComission/100)
+                else:
+                    betTrade = currentCapital * per;
+                winLossNum = random.uniform(0, 100)
+                if winLossNum > botPerc:
+                    profitPer = - random.uniform(0, maxTakeProfitPer)
+                else:
+                    profitPer = random.uniform(0, maxTakeProfitPer)
+                    winCount += 1
+                if isComission:
+                    finalTrade = betTrade * (1 + profitPer) * (1 - makerComission/100)
+                else:
+                    finalTrade = betTrade * (1 + profitPer)
+                currentCapital = currentCapital + finalTrade
+
+            ROI = (currentCapital - initialInvestment) / initialInvestment * 100
+            winPer = winCount / numTrades * 100
+            countPos += 1
+            results[countPos] = [botPerc, per * 100, round(ROI, 2), winPer]
+
+Results = pd.DataFrame(results, columns = ["Perc Won", "Bet Trade Perc", "ROI", "Win Perc"])
+pd.set_option('display.max_rows', None, 'display.max_columns', None)
+print(Results)
 
 sns.set_theme(style="dark")
 sns.set_palette("bright")
@@ -71,6 +78,3 @@ sns.set_palette("bright")
 #ax.legend(shadow=True, fontsize=16)
 
 #plt.show()
-
-print(ROI)
-print(perBetTrade)
